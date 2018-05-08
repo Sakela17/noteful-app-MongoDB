0ff7096b7e705475b53bf3982e7768b521838973
Marina Valiquette <marina.valiquette@gmail.com>
2018-05-08 09:39:27 -0400

    Add validations to POST /users

--- routes/users.js
+++ routes/users.js
@@ -20,33 +20,104 @@ const User = require('../models/user');
 
 /* ========== POST/CREATE USERS ========== */
 router.post('/', (req, res, next) => {
-  const { username, password, fullname = '' } = req.body;
-
   const requiredFields = ['username', 'password'];
 
-  const missingFields = requiredFields.find(field => {
-
+  const missingField = requiredFields.find(field => {
+    return !(field in req.body);
   });
 
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: `Missing ${missingField} in request body`,
+      location: missingField
+    });
+    // const err = new Error(`Missing ${missingField} in request body`);
+    // //Unprocessable entity
+    // err.status = 422;
+    // return next(err);
+  }
+  const stringFields = ['username', 'password', 'fullname'];
+  const nonStringField = stringFields.find(field => {
+    return field in req.body && typeof req.body[field] !== 'string';
+  });
 
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    })
+  }
+
+  const nonTrimmedField = stringFields.find(field => {
+    return req.body[field].trim() !== req.body[field];
+  });
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: `${nonTrimmedField} cannot start or end with a white space`,
+      location: nonTrimmedField
+    })
+  }
+
+  const sizedFields = {
+    username: {
+      min: 3
+    },
+    password: {
+      min: 3,
+      //bcrypt truncates string after 72 characters
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(field => {
+    return 'min' in sizedFields[field]  && req.body[field].length < sizedFields[field].min;
+  });
+
+  const tooLargeField = Object.keys(sizedFields).find(field => {
+    return 'max' in sizedFields[field]  && req.body[field].length > sizedFields[field].max;
+  });
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField ? `Must be at least ${sizedFields[tooSmallField].min} characters long` : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    })
+  }
+
+  const { username, password, fullname = '' } = req.body;
 
   return User.hashPassword(password)
     .then(digest => {
-      const newUser = {
+      return User.create({
         username,
         password: digest,
         fullname,
-      };
-      return User.create(newUser);
+      });
     })
     .then(result => {
       return res.status(201).location(`/api/users/${result.id}`).json(result);
     })
     .catch(err => {
+
       if (err.code === 11000) {
         err = new Error('The username already exists');
         err.status = 400;
       }
+      //
+      // if (err.reason === 'ValidationError') {
+      //   return res.status(err.code).json(err);
+      // }
+
+      res.status(500).json({code: 500, message: 'Internal server error'});
     });
 
 });
